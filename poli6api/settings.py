231939706ec5bd3b792705422211081968fd3dad
Arkan Leki <arkan.leki@gmail.com>
2022-09-24 00:58:16 +0300

    dbg

--- poli6api/settings.py
+++ poli6api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e!+e&#si58lgy#^#@038*j0zb95$ftr6r=$uxb9=%1447601by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
