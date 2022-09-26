d0007412caae1c244aa4b0a7e3076b042ca3a451
Arkan Leki <arkan.leki@gmail.com>
2022-09-26 21:44:50 +0300

    exteafor

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+# from django.utils.html import mark_safe # Newer versions
 from api.models import Account, Question, Answare, Quize, Result
 
 # Register your models here.
@@ -13,8 +13,14 @@ admin.site.register(Quize,QuizeAdmin)
 
 class AnswaresInline(admin.TabularInline):
     model = Answare
+    extra = 4
 
 class QuestionAdmin(admin.ModelAdmin):
+    # def content(self, obj):
+    #     return mark_safe(
+    #         '<a href="%s">%s</a>' % (obj.text, obj.text)
+    #     )
+    
     model = Question
     list_display = ('text', 'quize', 'correct_answare')
     list_filter = ['quize']
